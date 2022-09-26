2d959c38f8caceaa2858278c65c1325d904b2dde
Clint Maruti <clintmaruti@Clints-MacBook-Pro.local>
2022-09-26 11:13:34 +0300

    Editted Portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,51 +3,38 @@ import Navitem from "./Navitem";
 import me from "../img/me.jpeg";
 
 class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      NavItemActive: "",
-    };
-  }
-  activeitem = (x) => {
-    if (this.state.NavItemActive.length > 0) {
-      document
-        .getElementById(this.state.NavItemActive)
-        .classList.remove("active");
+    constructor(props) {
+        super(props);
+        this.state = {
+            NavItemActive: "",
+        };
     }
-    this.setState({ NavItemActive: x }, () => {
-      document.getElementById(this.state.NavItemActive).classList.add("active");
-    });
-  };
-  render() {
-    return (
-      <nav className="glass">
-        <img src={me} className="profilepic"></img>
-        <h2>
-          Tanyasis <mark>Mayiana</mark>
-        </h2>
-        <p>UI/UX Designer</p>
-        <ul>
-          <Navitem item="Home" tolink="/" activec={this.activeitem}></Navitem>
-          <Navitem
-            item="About"
-            tolink="/about"
-            activec={this.activeitem}
-          ></Navitem>
-          <Navitem
-            item="Portfolio"
-            tolink="/portfolio"
-            activec={this.activeitem}
-          ></Navitem>
-          <Navitem
-            item="Contact"
-            tolink="/contact"
-            activec={this.activeitem}
-          ></Navitem>
-        </ul>
-      </nav>
-    );
-  }
+    activeitem = (x) => {
+        if (this.state.NavItemActive.length > 0) {
+            document.getElementById(this.state.NavItemActive).classList.remove("active");
+        }
+        this.setState({ NavItemActive: x }, () => {
+            document.getElementById(this.state.NavItemActive).classList.add("active");
+        });
+    };
+    render() {
+        return (
+            <nav className="glass">
+                <img src={me} className="profilepic"></img>
+                <h2>
+                    Tanyasis <mark>Mayiana</mark>
+                </h2>
+                <p>Software Developer</p>
+                <p className="">UI/UX Designer</p>
+                <ul>
+                    <Navitem item="Home" tolink="/" activec={this.activeitem}></Navitem>
+                    <Navitem item="About" tolink="/about" activec={this.activeitem}></Navitem>
+                    <Navitem item="Portfolio" tolink="/portfolio" activec={this.activeitem}></Navitem>
+                    <Navitem item="Contact" tolink="/contact" activec={this.activeitem}></Navitem>
+                </ul>
+            </nav>
+        );
+    }
 }
 
 export default Navbar;
--- src/contents/Portfolio.js
+++ src/contents/Portfolio.js
@@ -7,46 +7,48 @@ import My_Girl from "../img/My_Girl.png";
 import Urban_food from "../img/Urban_food.png";
 
 class Portfolio extends Component {
-  render() {
-    return (
-      <div className="condiv">
-        <h1 className="subtopic">My Projects</h1>
-        <div className="hr_line line2 glass">
-          <div className="inner2 inner3"></div>
-        </div>
-        <div class="port_out port1 glass">
-          <a href="https://www.figma.com/file/3LAc4hjFnHIlaC6jjeNLLu/responsive-app">
-            <img src={Afia_rebrand} className="po1"></img>
-          </a>
-        </div>
-        <div class="port_out port2 glass">
-          <a href="https://www.figma.com/file/tmVQG8stAB71IDfi0ymYxy/CBC-curriculum">
-            <img src={CBC} className="po1"></img>
-          </a>
-        </div>
-        <div class="port_out port3 glass">
-          <a href="https://www.figma.com/file/ay1iEdtdGOD8b4wiGfF25g/Home-recovery">
-            <img src={Home_recovery} className="po1"></img>
-          </a>
-        </div>
-        <div class="port_out port4 glass">
-          <a href="https://www.figma.com/file/X0ZblBEmZIwa5mgVnuopmm/Music-app">
-            <img src={music_app} className="po1"></img>
-          </a>
-        </div>
-        <div class="port_out port4 glass">
-          <a href="https://www.figma.com/file/xM5szsGS06910CMBkCQloR/period-tracking">
-            <img src={My_Girl} className="po1"></img>
-          </a>
-        </div>
-        <div class="port_out port4 glass">
-          <a href="https://www.figma.com/file/2L7HqkvABpXiZ6IFQfKU5P/urban-food?node-id=0%3A1">
-            <img src={Urban_food} className="po1"></img>
-          </a>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="condiv">
+                <h1 className="subtopic">My Projects</h1>
+                <div className="hr_line line2 glass">
+                    <div className="inner2 inner3"></div>
+                </div>
+                <div className="portfolio_container">
+                    <div class="portfolio glass">
+                        <a href="https://www.figma.com/file/3LAc4hjFnHIlaC6jjeNLLu/responsive-app">
+                            <img src={Afia_rebrand} className="po1" alt="Afia Rebrand"></img>
+                        </a>
+                    </div>
+                    <div class="portfolio glass">
+                        <a href="https://www.figma.com/file/tmVQG8stAB71IDfi0ymYxy/CBC-curriculum">
+                            <img src={CBC} className="po1" alt="CBC"></img>
+                        </a>
+                    </div>
+                    <div class="portfolio glass">
+                        <a href="https://www.figma.com/file/ay1iEdtdGOD8b4wiGfF25g/Home-recovery">
+                            <img src={Home_recovery} className="po1" alt="Home recovery"></img>
+                        </a>
+                    </div>
+                    <div class="portfolio glass">
+                        <a href="https://www.figma.com/file/X0ZblBEmZIwa5mgVnuopmm/Music-app">
+                            <img src={music_app} className="po1" alt="music app"></img>
+                        </a>
+                    </div>
+                    <div class="portfolio glass">
+                        <a href="https://www.figma.com/file/xM5szsGS06910CMBkCQloR/period-tracking">
+                            <img src={My_Girl} className="po1" alt="My girl"></img>
+                        </a>
+                    </div>
+                    <div class="portfolio glass">
+                        <a href="https://www.figma.com/file/2L7HqkvABpXiZ6IFQfKU5P/urban-food?node-id=0%3A1">
+                            <img src={Urban_food} className="po1" alt="Urban Food"></img>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Portfolio;
